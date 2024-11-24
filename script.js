29082df44438725834613ccf76751c342cee4306
Cola Singleton <105658561+Colasingleton@users.noreply.github.com>
2024-11-23 19:45:51 -0500

    Update script.js
    changing path redirection

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ navLinks.forEach((link) => {
   link.addEventListener('click', (event) => {
     // Check if the element is an icon or contains a specific class
     if (link.classList.contains('home') || link.getAttribute('href') === '/') {
-      window.location.href = '/'; 
+      window.location.href = 'https://colasingleton.github.io/prototype/index.html'; 
     } else if (link.getAttribute('href') === '/checkout') {
       const confirmCheckout = confirm('Are you sure you want to checkout?');
       if (!confirmCheckout) {
